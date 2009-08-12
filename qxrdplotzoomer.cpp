c025c1c4a21f247e2305bd3e2aae62f247c1beda
jennings <>
2009-08-12 19:45:00 +0000

    *** empty log message ***

--- qxrdplotzoomer.cpp
+++ qxrdplotzoomer.cpp
@@ -1,35 +1,36 @@
 /******************************************************************
 *
-*  $Id: qxrdplotzoomer.cpp,v 1.3 2009/06/27 22:50:32 jennings Exp $
+*  $Id: qxrdplotzoomer.cpp,v 1.4 2009/08/12 19:44:59 jennings Exp $
 *
 *******************************************************************/
 
 #include "qxrdplotzoomer.h"
-#include "qxrdimageplot.h"
+#include "qxrdplot.h"
 
-QxrdPlotZoomer::QxrdPlotZoomer(QwtPlotCanvas *canvas, QxrdImagePlot *plot)
+QxrdPlotZoomer::QxrdPlotZoomer(QwtPlotCanvas *canvas, QxrdPlot *plot)
   : QwtPlotZoomer(canvas),
     m_Plot(plot),
-    SOURCE_IDENT("$Id: qxrdplotzoomer.cpp,v 1.3 2009/06/27 22:50:32 jennings Exp $")
+    SOURCE_IDENT("$Id: qxrdplotzoomer.cpp,v 1.4 2009/08/12 19:44:59 jennings Exp $")
 {
-  setTrackerMode(QwtPicker::ActiveOnly);
+  setTrackerMode(QwtPicker::AlwaysOn);
   setTrackerPen(QPen(Qt::green));
 }
 
 QwtText QxrdPlotZoomer::trackerText(const QwtDoublePoint &pos) const
 {
-  QxrdRasterData *raster = m_Plot->raster();
-
-  if (raster) {
-    return tr("%1, %2, %3").arg(pos.x()).arg(pos.y()).arg(raster->value(pos.x(),pos.y()));
+  if (m_Plot) {
+    return m_Plot -> trackerText(pos);
   } else {
-    return tr("%1, %2").arg(pos.x()).arg(pos.y());
+    return tr("(NULL)");
   }
 }
 
 /******************************************************************
 *
 *  $Log: qxrdplotzoomer.cpp,v $
+*  Revision 1.4  2009/08/12 19:44:59  jennings
+*  *** empty log message ***
+*
 *  Revision 1.3  2009/06/27 22:50:32  jennings
 *  Added standard log entries and ident macros
 *  Used standard property macros for acquisition parameters and image properties
