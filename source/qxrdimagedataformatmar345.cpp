4d1924e24a13fe9a0337eee3c457993389a6cc5e
jennings <jennings@anl.gov>
2010-11-09 15:21:02 -0600

    Fixed problems with loading mask data The image format factory had entries for <bool>, rather than <short>

--- source/qxrdimagedataformatmar345.cpp
+++ source/qxrdimagedataformatmar345.cpp
@@ -3,7 +3,7 @@
 static QxrdImageDataFormatMar345 fmt;
 static QcepImageDataFormatMar345<quint16> rawfmt("raw");
 static QcepImageDataFormatMar345<quint32> raw2fmt("raw2");
-static QcepImageDataFormatMar345<bool> maskfmt("mask");
+static QcepImageDataFormatMar345<short> maskfmt("mask");
 
 QxrdImageDataFormatMar345::QxrdImageDataFormatMar345(QString name)
   : QcepImageDataFormatMar345<double>(name)
