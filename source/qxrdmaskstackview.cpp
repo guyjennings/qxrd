e908136997527d94324686db1d64e7480cc18143
jennings <jennings@anl.gov>
2010-12-16 01:10:27 -0200

    Custom view class for mask stack

---
+++ source/qxrdmaskstackview.cpp
@@ -0,0 +1,62 @@
+#include "qxrdmaskstackview.h"
+#include <QListView>
+#include <QMenu>
+#include <QContextMenuEvent>
+
+QxrdMaskStackView::QxrdMaskStackView(QWidget *parent) :
+  QTableView(parent),
+  m_Dialog(NULL),
+  m_MaskStack(NULL)
+{
+}
+
+void QxrdMaskStackView::setMaskDialog(QxrdMaskDialog *dlg)
+{
+  m_Dialog = dlg;
+}
+
+void QxrdMaskStackView::setMaskStack(QxrdMaskStackPtr stk)
+{
+  m_MaskStack = stk;
+}
+
+void QxrdMaskStackView::contextMenuEvent(QContextMenuEvent *event)
+{
+  QMenu menu(NULL, NULL);
+
+  QAction *newMask = menu.addAction("New Mask Layer");
+  QAction *enbMask = menu.addAction("Enable Mask Layer(s)");
+  QAction *dsbMask = menu.addAction("Disable Mask Layer(s)");
+  QAction *delMask = menu.addAction("Delete Mask Layer(s)");
+  QAction *andMask = menu.addAction("AND Mask Layers");
+  QAction *orMask  = menu.addAction("OR Mask Layers");
+  QAction *thrMask = menu.addAction("Threshold Mask...");
+
+//  xLog->setCheckable(true);
+//  yLog->setCheckable(true);
+//  xLog->setChecked(get_XAxisLog());
+//  yLog->setChecked(get_YAxisLog());
+
+  QAction *action = menu.exec(event->globalPos());
+  QModelIndexList selected = selectedIndexes();
+
+  if (m_Dialog) {
+    if (action == newMask) {
+      m_MaskStack->newMask();
+    } else if (action == enbMask) {
+      m_MaskStack->enableMasks(selected);
+    } else if (action == dsbMask) {
+      m_MaskStack->disableMasks(selected);
+    } else if (action == delMask) {
+      m_MaskStack->deleteMasks(selected);
+    } else if (action == andMask) {
+      m_MaskStack->andMasks(selected);
+    } else if (action == orMask) {
+      m_MaskStack->orMasks(selected);
+    } else if (action == thrMask) {
+//      m_MaskStack->thresholdMasks(selected);
+    }
+  }
+
+  event->accept();
+}
---
+++ source/qxrdmaskstackview.h
@@ -0,0 +1,26 @@
+#ifndef QXRDMASKSTACKVIEW_H
+#define QXRDMASKSTACKVIEW_H
+
+#include <QTableView>
+#include "qxrdmaskstack.h"
+#include "qxrdmaskdialog.h"
+
+class QxrdMaskStackView : public QTableView
+{
+  Q_OBJECT
+public:
+  explicit QxrdMaskStackView(QWidget *parent = 0);
+  void setMaskStack(QxrdMaskStackPtr stk);
+  void setMaskDialog(QxrdMaskDialog *dlg);
+  void contextMenuEvent ( QContextMenuEvent * event );
+
+signals:
+
+public slots:
+
+private:
+  QxrdMaskDialog  *m_Dialog;
+  QxrdMaskStackPtr m_MaskStack;
+};
+
+#endif // QXRDMASKSTACKVIEW_H
