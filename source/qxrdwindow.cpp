12507968f650f306df3f28667619efa42b3c5d9e
jennings <jennings@anl.gov>
2011-01-29 08:41:34 -0600

    Add sync acquisition widget to dock after it is created

--- source/qxrdwindow.cpp
+++ source/qxrdwindow.cpp
@@ -93,6 +93,7 @@ QxrdWindow::QxrdWindow(QxrdApplicationPtr app, QxrdAcquisitionPtr acq, QxrdDataP
   m_MaskDockWidget -> setWidget(m_MaskDialog);
 
   m_SynchronizedAcquisitionDialog = new QxrdSynchronizedAcquisitionDialog(this, m_Acquisition);
+  addDockWidget(Qt::RightDockWidgetArea, m_SynchronizedAcquisitionDialog);
 
   //  m_Calculator = new QxrdImageCalculator(m_DataProcessor);
   //  addDockWidget(Qt::RightDockWidgetArea, m_Calculator);
