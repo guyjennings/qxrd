2396d6e3980588a38cf56488f6d2568caed29a9d
jennings <jennings@anl.gov>
2011-06-15 16:00:43 -0500

    Debug message when starting file broeser model updater

--- source/qxrdfilebrowsermodelupdater.cpp
+++ source/qxrdfilebrowsermodelupdater.cpp
@@ -1,9 +1,13 @@
 #include "qxrdfilebrowsermodelupdater.h"
+#include "qxrdapplication.h"
 #include <QThread>
 
 QxrdFileBrowserModelUpdater::QxrdFileBrowserModelUpdater(QxrdFileBrowserModel *browser, QObject *parent) :
     QObject(parent)
 {
+  if (qcepDebug(DEBUG_THREADS)) {
+    g_Application->printMessage("Starting Browser Model Updater");
+  }
 }
 
 void QxrdFileBrowserModelUpdater::shutdown()
