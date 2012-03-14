3bc3b3f10198dda34b74401f53b0e9dad9b2925a
jennings <jennings@anl.gov>
2012-03-14 11:43:38 -0500

    Added properties to QxrdAcquisitionExtraInputs

--- source/qxrdacquisitionextrainputs.cpp
+++ source/qxrdacquisitionextrainputs.cpp
@@ -4,7 +4,12 @@
 
 QxrdAcquisitionExtraInputs::QxrdAcquisitionExtraInputs(QxrdSettingsSaverWPtr saver, QxrdExperimentWPtr doc, QxrdAcquisitionWPtr acq) :
   QObject(acq.data()),
-  m_ExtraInputs(saver, this, "extraInputs", 8),
+  m_ExtraInputs(saver, this, "extraInputs", 0),
+  m_ChannelNames(saver, this, "channelNames", QStringList()),
+  m_SampleRate(saver, this, "sampleRate", 1000.0),
+  m_StartOffset(saver, this, "startOffset", 0.0),
+  m_EndOffset(saver, this, "endOffset", 0.0),
+  m_AcquireDelay(saver, this, "acquireDelay", 0.107),
   m_Experiment(doc),
   m_Acquisition(acq)
 {
--- source/qxrdacquisitionextrainputs.h
+++ source/qxrdacquisitionextrainputs.h
@@ -29,6 +29,21 @@ public:
   Q_PROPERTY(int extraInputs READ get_ExtraInputs WRITE set_ExtraInputs)
   QCEP_INTEGER_PROPERTY(ExtraInputs)
 
+  Q_PROPERTY(QStringList channelNames READ get_ChannelNames WRITE set_ChannelNames)
+  QCEP_STRING_LIST_PROPERTY(ChannelNames)
+
+  Q_PROPERTY(double sampleRate READ get_SampleRate WRITE set_SampleRate)
+  QCEP_DOUBLE_PROPERTY(SampleRate)
+
+  Q_PROPERTY(double startOffset READ get_StartOffset WRITE set_StartOffset)
+  QCEP_DOUBLE_PROPERTY(StartOffset)
+
+  Q_PROPERTY(double endOffset READ get_EndOffset WRITE set_EndOffset)
+  QCEP_DOUBLE_PROPERTY(EndOffset)
+
+  Q_PROPERTY(double acquireDelay READ get_AcquireDelay WRITE set_AcquireDelay)
+  QCEP_DOUBLE_PROPERTY(AcquireDelay)
+
 private:
   mutable QMutex              m_Mutex;
   QxrdExperimentWPtr          m_Experiment;
