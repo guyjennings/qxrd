c45951902fcda3ef297a70087f8f5b6d38256a19
jennings <jennings@anl.gov>
2012-03-26 14:32:46 -0500

    Added weak pointer to PE plugin interface

--- source/qxrdperkinelmerplugininterface-ptr.h
+++ source/qxrdperkinelmerplugininterface-ptr.h
@@ -1,8 +1,11 @@
 #ifndef QXRDPERKINELMERPLUGININTERFACE_PTR_H
 #define QXRDPERKINELMERPLUGININTERFACE_PTR_H
 
+#include <QSharedPointer>
+
 class QxrdPerkinElmerPluginInterface;
 
 typedef QSharedPointer<QxrdPerkinElmerPluginInterface> QxrdPerkinElmerPluginInterfacePtr;
+typedef QWeakPointer<QxrdPerkinElmerPluginInterface> QxrdPerkinElmerPluginInterfaceWPtr;
 
 #endif // QXRDPERKINELMERPLUGININTERFACE_PTR_H
