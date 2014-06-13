08240981efc3df1e127c465978d775c01c481978
jennings <jennings@anl.gov>
2014-06-13 12:25:50 -0500

    Link P0,P1 and P2 parameters in distortion correction

--- source/qxrddistortioncorrectiondialog.cpp
+++ source/qxrddistortioncorrectiondialog.cpp
@@ -14,7 +14,9 @@ QxrdDistortionCorrectionDialog::QxrdDistortionCorrectionDialog(QxrdDistortionCor
   setupUi(this);
 
   m_DistortionCorrection->prop_DistortionImagePath()->linkTo(m_DistortionCalibrationPath);
-//  m_DistortionCorrection->prop_DistortionP0()->linkTo();
+  m_DistortionCorrection->prop_DistortionP0()->linkTo(m_DistortionP0X, m_DistortionP0Y);
+  m_DistortionCorrection->prop_DistortionP1()->linkTo(m_DistortionP1X, m_DistortionP1Y);
+  m_DistortionCorrection->prop_DistortionP2()->linkTo(m_DistortionP2X, m_DistortionP2Y);
 }
 
 void QxrdDistortionCorrectionDialog::init(QxrdDistortionCorrectionDialogSettingsWPtr settings)
