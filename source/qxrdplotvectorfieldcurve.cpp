7f7dc86552d36ca8907145f32990d6b6134bd21d
jennings <jennings@anl.gov>
2014-06-25 15:09:45 -0500

    Simple implementation of vector field plot

--- source/qxrdplotvectorfieldcurve.cpp
+++ source/qxrdplotvectorfieldcurve.cpp
@@ -1,4 +1,6 @@
 #include "qxrdplotvectorfieldcurve.h"
+#include "qwt_painter.h"
+#include"qwt_scale_map.h"
 
 QxrdPlotVectorFieldCurve::QxrdPlotVectorFieldCurve()
 {
@@ -16,5 +18,32 @@ void QxrdPlotVectorFieldCurve::drawSeries
    const QRectF &canvasRect,
    int from, int to) const
 {
+  if (to < 0) {
+    to = dataSize() - 1;
+  }
 
+  if (from < 0) {
+    from = 0;
+  }
+
+  if (from > to) {
+    return;
+  }
+
+  painter->save();
+
+  const QwtSeriesData<QxrdPoint4D> *series = data();
+
+  for (int i=from; i<=to; i++) {
+    const QxrdPoint4D sample = series->sample(i);
+
+    double x0 = xMap.transform(sample.x());
+    double y0 = yMap.transform(sample.y());
+    double x1 = xMap.transform(sample.x()+sample.z()*50.0);
+    double y1 = yMap.transform(sample.y()+sample.t()*50.0);
+
+    QwtPainter::drawLine(painter, x0, y0, x1, y1);
+  }
+
+  painter->restore();
 }
