5195c9ea630ae896446b3060ea50531247ffe1fc
Guy Jennings <gjennings@earthlink.net>
2015-06-07 09:04:04 -0500

    Remove compiler warning on win32

--- qceplib-base/qcepsettingssaver.cpp
+++ qceplib-base/qcepsettingssaver.cpp
@@ -58,7 +58,7 @@ void QcepSettingsSaver::performSave()
   }
 }
 
-void QcepSettingsSaver::changed(QcepProperty */*prop*/)
+void QcepSettingsSaver::changed(QcepProperty * /*prop*/)
 {
   m_ChangeCount.fetchAndAddOrdered(1);
 }
