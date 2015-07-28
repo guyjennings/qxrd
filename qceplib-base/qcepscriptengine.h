12e12ccbbb9a9758f3da79d5d4ca5a28e183975f
Guy Jennings <gjennings@earthlink.net>
2015-07-27 19:43:57 -0500

    Moved script function help text functions from QxrdScriptEngine into QcepScriptEngine

--- qceplib-base/qcepscriptengine.h
+++ qceplib-base/qcepscriptengine.h
@@ -7,6 +7,17 @@ class QcepScriptEngine : public QScriptEngine
 {
 public:
   QcepScriptEngine(QObject *parent = 0);
+
+public slots:
+  QString documentationLink(QString base, QString subItem);
+  QByteArray helpText(QString item);
+
+  QString documentationText(QString item);
+  QString documentationText(QRegExp pattern);
+
+private:
+  QString tableHeader();
+  QString tableFooter();
 };
 
 #endif // QCEPSCRIPTENGINE_H
