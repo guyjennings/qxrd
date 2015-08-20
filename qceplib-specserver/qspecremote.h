876b7fc2c51f0f18a5cbe6d241889f5124ababe4
jennings <jennings@anl.gov>
2015-08-20 15:23:01 -0500

    Update to new version of spec_server (6.2) Add QSpecRemote class

---
+++ qceplib-specserver/qspecremote.h
@@ -0,0 +1,58 @@
+#ifndef QSPECREMOTE_H
+#define QSPECREMOTE_H
+
+#include <QObject>
+#include <QString>
+#include <QTcpSocket>
+#include <QVariant>
+
+struct svr_head;
+
+class QSpecRemote : public QObject
+{
+  Q_OBJECT
+
+public:
+  QSpecRemote(QObject *parent=NULL, const char *name=0);
+  virtual ~QSpecRemote();
+  int close();
+  int abort();
+
+  int connectToHost (const QString &host, const QString &portname=QString::null);
+  int sendHello();
+  QVariant getProperty(const QString &prop);
+  void     watchProperty(const QString &prop);
+  void     setProperty(const QString &prop, const QVariant &value);
+  void     watchMotor(const QString &motor);
+  void     sendCmd(const QString &cmd);
+  QVariant sendCmdReply(const QString &cmd);
+
+private:
+  void init();
+  QVariant getReturnValue();
+  void     dispatchPacket();
+
+  void connectReadyRead();
+  void disconnectReadyRead();
+
+private slots:
+ void hostFound();
+ void connected();
+ void disconnected();
+ void readyRead();
+ void bytesWritten(qint64);
+ void error( QAbstractSocket::SocketError );
+
+signals:
+  void socketStateChanged(int state);
+  void statusMessage(const QString &msg);
+
+private:
+  unsigned         m_SerialNumber;
+  bool             m_ExpectHeader;
+  QTcpSocket      *m_SpecSocket;
+  struct svr_head *m_Packet;
+  QByteArray       m_Data;
+};
+
+#endif // QSPECREMOTE_H
