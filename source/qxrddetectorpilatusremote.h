98f390beaca2665321cab6e0aeb5580b93eceb3b
jennings <jennings@anl.gov>
2015-09-24 15:00:32 -0500

    Added file extension parameter to detectors

---
+++ source/qxrddetectorpilatusremote.h
@@ -0,0 +1,15 @@
+#ifndef QXRDDETECTORPILATUSREMOTE_H
+#define QXRDDETECTORPILATUSREMOTE_H
+
+
+class QxrdDetectorPilatusRemote : public QcepObject
+{
+public:
+  QxrdDetectorPilatusRemote();
+
+signals:
+
+public slots:
+};
+
+#endif // QXRDDETECTORPILATUSREMOTE_H
