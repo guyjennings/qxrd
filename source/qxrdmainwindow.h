db38d29d320966b59a856db83e4ceb90b06d9050
jennings <jennings@anl.gov>
2015-09-29 17:05:22 -0500

    Moved most of QxrdMainWindow into QcepMainWindow Bumped version to 0.11.14

--- source/qxrdmainwindow.h
+++ source/qxrdmainwindow.h
@@ -1,31 +1,13 @@
 #ifndef QXRDMAINWINDOW_H
 #define QXRDMAINWINDOW_H
 
-#include <QMainWindow>
-#include <QObject>
-#include <QDateTime>
+#include "qcepmainwindow.h"
 
-class QxrdMainWindow : public QMainWindow
+class QxrdMainWindow : public QcepMainWindow
 {
   Q_OBJECT
 public:
   explicit QxrdMainWindow(QWidget *parent = 0);
-
-  void possiblyClose();
-  bool wantToClose();
-
-signals:
-
-public slots:
-  void shrinkPanels(int fontSize, int spacing);
-  void setFontSize(int fontSize);
-  void setSpacing(int spacing);
-  virtual void printMessage(QString msg, QDateTime ts=QDateTime::currentDateTime()) = 0;
-
-protected:
-  void shrinkDockWidget(QDockWidget *dockWidget, int fontSize, int spacing);
-  void shrinkObject(QObject *obj, int fontSize, int spacing);
-  void setObjectSpacing(QObject *obj, int spacing);
 };
 
 #endif // QXRDMAINWINDOW_H
