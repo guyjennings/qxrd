cdec2c200dc9fae532a6106bead5f067064d3916
jennings <jennings@anl.gov>
2017-06-22 13:32:26 -0500

    Modify qcepH5Version to use C strings, not QString

--- qceplib-base/qcepmacros.cpp
+++ qceplib-base/qcepmacros.cpp
@@ -11,7 +11,8 @@ const char* qceplibVersion()
 
 #include "hdf5.h"
 
-QString _hdfVersion;
+//QString _hdfVersion;
+static char _hdfVersion[256];
 
 const char* qcepH5Version()
 {
@@ -19,9 +20,10 @@ const char* qcepH5Version()
 
   H5get_libversion(&maj, &min, &rel);
 
-  _hdfVersion = QObject::tr("%1.%2.%3").arg(maj).arg(min).arg(rel);
+//  _hdfVersion = QObject::tr("%1.%2.%3").arg(maj).arg(min).arg(rel);
+  snprintf(_hdfVersion, sizeof(_hdfVersion), "%d.%d.%d", maj, min, rel);
 
-  return qPrintable(_hdfVersion);
+  return _hdfVersion;
 }
 
 #else
