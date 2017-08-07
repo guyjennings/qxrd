3216d65924444b2737df875df312b9568f5af94f
Guy Jennings <gjennings@earthlink.net>
2017-08-07 07:52:23 -0500

    Added headerData to QcepImageDataSpreadsheetModel

--- qceplib-base/qcepimagedataspreadsheetmodel.h
+++ qceplib-base/qcepimagedataspreadsheetmodel.h
@@ -14,6 +14,8 @@ public:
   int columnCount(const QModelIndex &parent = QModelIndex()) const;
   QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
 
+  QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+
 private:
   QWeakPointer < QcepImageDataBase > m_Image;
 };
