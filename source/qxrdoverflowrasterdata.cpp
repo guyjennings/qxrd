430180b142d985cc73ae9c8ab0331613a23ef65d
jennings <jennings@anl.gov>
2017-10-06 16:31:35 -0500

    Added QxrdOverflowRasterData to display overflow pixels

--- source/qxrdoverflowrasterdata.cpp
+++ source/qxrdoverflowrasterdata.cpp
@@ -1,6 +1,40 @@
+#include "qxrddebug.h"
 #include "qxrdoverflowrasterdata.h"
+#include "qxrdimagedata.h"
+#include "qxrdapplication.h"
 
-QxrdOverflowRasterData::QxrdOverflowRasterData()
+QxrdOverflowRasterData::QxrdOverflowRasterData(QxrdDoubleImageDataPtr image, double level)
+  : QwtRasterData(),
+    m_Image(image),
+    m_OverflowLevel(level)
 {
+  if (g_Application && qcepDebug(DEBUG_IMAGES)) {
+    g_Application->printMessage(QObject::tr("QxrdOverflowRasterData::QxrdMaskRasterData(%1) [%2]")
+                                .HEXARG(image.data()).HEXARG(this));
+  }
 
+  setInterval(Qt::XAxis, QwtInterval(0, (image?image->get_Width():0)));
+  setInterval(Qt::YAxis, QwtInterval(0, (image?image->get_Height():0)));
+  setInterval(Qt::ZAxis, QwtInterval(0,3));
+
+  if (image) {
+    if (image->get_SummedExposures()>1) {
+      m_OverflowLevel = level*image->get_SummedExposures();
+    }
+  }
+}
+
+double QxrdOverflowRasterData::value(double x, double y) const
+{
+  double res = 0;
+
+  if (m_Image) {
+    double val = m_Image->value((int) qRound(x), (int) qRound(y));
+
+    if (val == val && val > m_OverflowLevel) {
+      res = 1;
+    }
+  }
+
+  return res;
 }
--- source/qxrdoverflowrasterdata.h
+++ source/qxrdoverflowrasterdata.h
@@ -1,11 +1,22 @@
 #ifndef QXRDOVERFLOWRASTERDATA_H
 #define QXRDOVERFLOWRASTERDATA_H
 
+#include "qcepmacros.h"
+
+#include <qwt_raster_data.h>
+#include "qxrdimagedata-ptr.h"
 
 class QxrdOverflowRasterData : public QwtRasterData
 {
 public:
-  QxrdOverflowRasterData();
+  QxrdOverflowRasterData(QxrdDoubleImageDataPtr image, double level);
+
+public:
+  double value(double x, double y) const;
+
+private:
+  QxrdDoubleImageDataPtr m_Image;
+  double                 m_OverflowLevel;
 };
 
 #endif // QXRDOVERFLOWRASTERDATA_H
