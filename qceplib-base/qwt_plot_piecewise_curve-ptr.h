da0e69d6689703f7e4adb627baa293db6622fe00
jennings <jennings@anl.gov>
2017-10-09 14:14:15 -0500

    Added qwt_plot_piecewise_curve-ptr.h

---
+++ qceplib-base/qwt_plot_piecewise_curve-ptr.h
@@ -0,0 +1,11 @@
+#ifndef QWT_PLOT_PIECEWISE_CURVEPTR_H
+#define QWT_PLOT_PIECEWISE_CURVEPTR_H
+
+#include <QSharedPointer>
+
+class QwtPlotPiecewiseCurve;
+
+typedef QSharedPointer<QwtPlotPiecewiseCurve> QwtPlotPiecewiseCurvePtr;
+typedef QWeakPointer<QwtPlotPiecewiseCurve>   QwtPlotPiecewiseCurveWPtr;
+
+#endif // QWT_PLOT_PIECEWISE_CURVEPTR_H
