aa036d5f307d9ac596e0b0610559e24b7045c2b3
jennings <jennings@anl.gov>
2018-01-20 21:10:48 -0600

    Added powder point on ring and trace powder ring commands

--- libraries/qxrdlib/qxrdpowderpointscommand.cpp
+++ libraries/qxrdlib/qxrdpowderpointscommand.cpp
@@ -6,12 +6,18 @@
 QxrdPowderPointsCommand::QxrdPowderPointsCommand(QString name,
                                                  QxrdPlotWidget *plot,
                                                  QxrdPlotWidgetSettingsWPtr set)
-  : QxrdPlotButtonCommand(name, plot, set, ":/images/powder_point.png", "Powder Points", true)
+  : QxrdPlotButtonCommand(name, plot, set, QString(), QString(), true)
 {  
+  appendMode(":/images/powder_point.png",      "Powder Points");
+  appendMode(":/images/powder_point_ring.png", "Powder Point on Ring");
+  appendMode(":/images/powder_trace_ring.png", "Trace Points on Ring");
+
   m_Picker = new QxrdPowderPointPicker(m_Plot);
   m_Picker -> setEnabled(false);
 
   setPlotPicker(m_Picker);
+
+  changeMode(0);
 
   connect(m_Picker, (void (QwtPlotPicker::*)(const QPointF&)) &QwtPlotPicker::selected,
           this, &QxrdPowderPointsCommand::selected);
